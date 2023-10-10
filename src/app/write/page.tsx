08ab87c3a6a94aeb521724bf7588aa025228c2c9
Matthew Araujo <matthewaraujo20@gmail.com>
2023-10-10 16:40:21 -0300

    write page

---
+++ src/app/write/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Image, ImageIcon, PlusCircle, Upload, Video } from "lucide-react";
+
+import "react-quill/dist/quill.bubble.css";
+
+import ReactQuill from "react-quill";
+import React from "react";
+
+function FuncionalitesButtons() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="shadow-sm">
+        <PlusCircle />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="flex">
+        <DropdownMenuItem>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Video />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Add a video</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Upload />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Add a file</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <ImageIcon />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Add a photo</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+export default function WritePage() {
+  const [value, setValue] = React.useState("");
+  return (
+    <div className="flex flex-col relative">
+      <Input placeholder="Title" className="p-12 text-5xl border-none outline-none bg-transparent text-slate-500 placeholder:text-slate-300"/>
+      <div>
+        <FuncionalitesButtons />
+
+        <ReactQuill
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your Story..."
+          className="min-h-96 max-h-fit text-xl"
+        />
+      </div>
+      <button className="py-2 px-5 border-none cursor-pointer text-slate-100 bg-green-800 w-fit rounded-full mt-20">Publish</button>
+    </div>
+  );
+}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
         <Image src="/tiktok.png" alt="facebook" width={24} height={24} />
         <Image src="/youtube.png" alt="facebook" width={24} height={24} />
       </div>
-      <div className="flex-1 text-left text-2xl font-bold md:text-4xl md:text-center  ">Farinha</div>
+      <div className="flex-1 text-left text-2xl font-bold md:text-4xl md:text-center  "><Link href="/">Axl Pedro Blog</Link></div>
       <div className="flex justify-end md:justify-center gap-4  flex-1 text-lg xl:text-xl xl:gap-5">
         <Link className="hidden md:block" href="/">Inicio</Link>
         <Link className="hidden md:block" href="/contato">Contato</Link>
